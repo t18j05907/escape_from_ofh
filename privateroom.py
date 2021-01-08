ba1f5ca356300420f6f45859da22d660cdc245d1
t18j055ty <t18j055ty@ous.jp>
2021-01-08 15:44:50 +0900

    tomiyama

--- privateroom.py
+++ privateroom.py
@@ -11,10 +11,34 @@ from room_base import Room_Base
 class Privateroom(Room_Base):
     def __init__(self, screen, lock_flag):
         super().__init__(screen, lock_flag)
-        self.img_room = pygame.image.load("res/privateroom/privateroom.png")
+        self.img_room = pygame.image.load("res/privateroom/perfect_solution_pr.png")
+        self.img_shelf = pygame.image.load("res/privateroom/shelf.png")
+        self.img_wallpaper = pygame.image.load("res/privateroom/wallpaper.png")
+        self.img_speaker = pygame.image.load("res/privateroom/speaker.png")
+        self.shelf = False
+        self.wallpaper = False
+        self.speaker = False
+        
         
     def click_event(self, x, y):
-        pass
+        if 1079 < x < 1171 and 279 < y < 373:
+            self.shelf = not self.shelf
+            
+        if 683 < x < 773 and 455 < y < 565:
+            self.wallpaper = not self.wallpaper
+            
+        if 471 < x < 523 and 751 < y < 857:
+            self.speaker = not self.speaker
+            
             
     def draw(self):
         self.screen.blit(self.img_room, [0, 0])
+        
+        if self.shelf == False:
+            self.screen.blit(self.img_shelf, [1079,279])
+            
+        if self.wallpaper == False:
+            self.screen.blit(self.img_wallpaper, [683,455])
+            
+        if self.speaker == False:
+            self.screen.blit(self.img_speaker, [471,751])
